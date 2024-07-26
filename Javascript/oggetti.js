72ace51757ca5176ced3c6bc9b736f572dc164d8
Giancosimo <giancx91@gmail.com>
2024-07-26 14:18:54 +0200

    file

---
+++ Javascript/oggetti.js
@@ -0,0 +1,8 @@
+let car = {
+  name: "Punto",
+  color: "Blue",
+};
+
+for (let key in car) {
+  console.log(key, car[key]);
+}
---
+++ Javascript/test.js
@@ -0,0 +1,54 @@
+/*const name = "Giancosimo";
+const surname = "Calabrese";
+let age = 33;
+let className = "fs30";
+
+function older(eta) {
+  if (eta <= 30) {
+    console.log("it Is Old");
+  } else {
+    console.log("not true");
+  }
+}
+older(33)
+older(19)
+older()*/
+
+/*function calcoloCassico(a, b, c) {
+  a + b + c;
+}
+let calcolo = (a, b, c) => a + b + c;
+let risultato = calcolo(10, 20, 15);
+console.log(risultato);
+
+console.log(calcolo(risultato, 22, 15));*/
+
+/*function sayHelloName(callback) {
+  console.log("Hello ");
+  callback();
+}
+
+function printName() {
+  console.log("Giancosimo");
+}
+
+sayHelloName(printName);*/
+
+/*setTimeout(function () {
+  console.log("Hello", "Giancosimo");
+}, 1000);*/
+
+/*let sum = (a, b) => a + b;
+
+console.log(sum(5, 5));*/
+
+function canPlay() {
+  var sport = " Football";
+
+  if (true) {
+    let personName = "Cosimo";
+    console.log(personName + sport);
+  }
+}
+
+canPlay();
