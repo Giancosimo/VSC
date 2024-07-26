df0b1488ac9f309288fe4a35b7f8e1c62964dd4c
Giancosimo <giancx91@gmail.com>
2024-07-26 14:18:54 +0200

    file

---
+++ Javascript/comparazione-variabili.js
@@ -0,0 +1,67 @@
+let heightRectangle = 10;
+let widthRectangle = 30;
+console.log(heightRectangle, widthRectangle);
+
+let circleRadius = 50;
+
+let pizzaPrice = 3.5;
+let pasticciottoPrice = 2;
+let rusticoPrice = 2.5;
+
+console.log(pizzaPrice, pasticciottoPrice, rusticoPrice);
+
+let number = (2 + 4) * 5;
+console.log(number);
+
+let rectangleHeight = 10;
+let rectangleWidth = 30;
+let rectangleArea = rectangleHeight * rectangleWidth;
+console.log(rectangleArea);
+
+let nome = "Giancosimo";
+let surname = "Calabrese";
+let fullName = nome + " " + surname;
+console.log(fullName);
+
+let itsSunny = true;
+let itsRaining = false;
+let ineedmyUmbrella = false;
+let itsWarm = true;
+let itsCold = false;
+let season = (itsSunny, !itsRaining, !ineedmyUmbrella, itsWarm, !itsCold);
+console.log(season);
+
+let hello = "Ciao"; //boolean
+let age = 18; //string
+let isGraduated = false; //number
+
+console.log(Boolean(hello));
+console.log(String(age));
+console.log(Number(isGraduated));
+
+
+/* console.log(10 < 11);
+console.log(45 == 45);
+console.log(true === true);
+console.log(false === false);
+console.log(1 == 1);
+console.log(4 <= 6);
+console.log(4 >= 2); */
+
+const firstName = "Mario";
+const lastname = "Rossi";
+const age = 27;
+const isGraduated = false;
+const average = 27;
+const examsCompleted = 10;
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+console.log(firstName == "Mario");
+console.log(lastname != "Rossi");
+console.log(age <= 27);
+console.log(isGraduated > false);
+console.log(average < 27);
+console.log(examsCompleted == 10);
+console.log(firstYearCompleted != true);
+console.log(yearsCompleted != 1);
---
+++ Javascript/operatori-logici.js
@@ -0,0 +1,74 @@
+/*const isSunnyDay = true;
+const isAutumn = false;
+const itIsRaining = false;
+const isSummer = true;
+
+console.log(isSunnyDay && isSummer);
+console.log(isAutumn || isSummer);
+console.log(isSunnyDay || !itIsRaining);
+console.log(isSummer && itIsRaining);
+console.log(isSunnyDay && itIsRaining);
+console.log(!isSummer && itIsRaining);*/
+
+/*let pin = 1000;
+if (pin > 999 && pin <= 10000) {
+  console.log("pin is valid");
+} else {
+  console.log("pin is not valid");
+}*/
+
+/*let isSunnyDay = true;
+let isHomeworkCompleted = true;
+let result =
+  isSunnyDay && isHomeworkCompleted
+    ? "Jesse can go out to play"
+    : "Jesse stays at home";
+console.log(result);*/
+
+/*let favoriteFood = "pizza";
+switch (favoriteFood) {
+  case "pizza":
+    console.log("You like PIZZA");
+    break;
+  case "coconut":
+    console.log("You like COCONUT");
+    break;
+
+  default:
+    console.log("You like something else");
+    break;
+}*/
+
+/*for (let i = -10; i <= 0; i++) {
+  console.log(i);
+}*/
+
+/*for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}*/
+/*let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}*/
+
+/*for (let i = 0; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log("numeriPari", i);
+  }
+}*/
+/*let somma = 0;
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+  somma += i;
+}
+console.log(somma);*/
+
+/*function somma(name) {
+  let result = "hello " + name;
+  return result;
+}
+let risultato = somma("John");
+console.log(risultato);*/
+
+
